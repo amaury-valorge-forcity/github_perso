03b23ef055d2aea4833095e6d3055a9ea0f46478
amaury-valorge-forcity <amaury.valorge@forcity.com>
2015-12-16 01:06:15 +0100

    RAS

--- QGIS/Data_convert/Index_Spatial.py
+++ QGIS/Data_convert/Index_Spatial.py
@@ -13,14 +13,14 @@ import qgis
 import unicodedata
 import subprocess
 
-''' recuperation les couches (valeurs qgis) ouvertes dans la session et boucle'''
+""" recuperation les couches (valeurs qgis) ouvertes dans la session et boucle"""
 count=0
 for layer in QgsMapLayerRegistry.instance().mapLayers().values():
-	''' recuperation du nom du fichier '''
+	""" recuperation du nom du fichier """
 	pathlayer=layer.dataProvider().dataSourceUri()
 	pathlayer=pathlayer.split('|')
 	output=pathlayer[0]
-	''' creation dun index spatial qix '''
+	""" creation dun index spatial qix """
 	vct = QgsVectorLayer(output, "tmp", "ogr" )
 	provider = vct.dataProvider()
 	provider.createSpatialIndex()
